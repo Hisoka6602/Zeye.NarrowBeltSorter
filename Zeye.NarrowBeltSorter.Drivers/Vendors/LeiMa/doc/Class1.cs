2aa8853cc60d16b2059a99055a7aa4474f0e546d
Hisoka6602 <91233659+Hisoka6602@users.noreply.github.com>
2026-03-23 20:25:44 +0800

    雷玛

---
+++ Zeye.NarrowBeltSorter.Drivers/Vendors/LeiMa/doc/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Zeye.NarrowBeltSorter.Drivers.Vendors.LeiMa.doc;
+
+internal class Class1 {
+}
