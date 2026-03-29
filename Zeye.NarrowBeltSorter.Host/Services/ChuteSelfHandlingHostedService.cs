fb13e73238fc467d9b0a0837602a7a237469071a
Hisoka6602 <91233659+Hisoka6602@users.noreply.github.com>
2026-03-29 15:51:26 +0800

    格口自理服务

---
+++ Zeye.NarrowBeltSorter.Host/Services/ChuteSelfHandlingHostedService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Zeye.NarrowBeltSorter.Host.Services {
+
+    public class ChuteSelfHandlingHostedService : BackgroundService {
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken) {
+            return null;
+        }
+    }
+}
