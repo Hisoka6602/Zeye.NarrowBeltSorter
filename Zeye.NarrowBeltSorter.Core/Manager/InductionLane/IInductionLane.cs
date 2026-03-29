fc58a393b9d8ac53df50c0aeb19435d9f50beb86
Hisoka6602 <91233659+Hisoka6602@users.noreply.github.com>
2026-03-29 16:49:38 +0800

    定义

---
+++ Zeye.NarrowBeltSorter.Core/Manager/InductionLane/IInductionLane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Zeye.NarrowBeltSorter.Core.Manager.InductionLane {
+
+    /// <summary>
+    /// 供包台
+    /// </summary>
+    public interface IInductionLane {
+        //-----------字段--------------
+        //供包台Id
+        //供包台名称
+        //当前连接状态
+        //创建包裹到上车位距离（mm）
+        //皮带速度
+        //是否首次稳速后再启动
+        //供包台皮带IO(集合)
+        //供包台状态变化
+        //创建包裹Io(SensorInfo类型)
+        //是否监控包裹长度
+        //-----------事件--------------
+        //包裹创建事件
+        //包裹到达上车位事件
+        //供包台状态变化事件
+        //-----------方法--------------
+        //设置供包台配置
+        //启动供包台
+        //停止供包台
+    }
+}
---
+++ Zeye.NarrowBeltSorter.Core/Manager/Signal/ISignalTower.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Zeye.NarrowBeltSorter.Core.Manager.Signal {
+
+    /// <summary>
+    /// 信号塔接口，定义了信号塔的基本功能和行为。
+    /// </summary>
+    public interface ISignalTower {
+        //------------字段------------
+
+        //红灯IO(SensorInfo)
+        //绿灯IO(SensorInfo)
+        //蜂鸣器IO(SensorInfo)
+        //黄灯IO(SensorInfo)
+
+        //当前三色灯状态
+        //当前蜂鸣器状态
+        //当前连接状态
+
+        //------------事件------------
+        // 三色灯状态变更事件
+        // 蜂鸣器状态变更事件
+        // 连接状态变更事件
+
+        //------------方法------------
+
+        //设置三色灯状态
+        //设置蜂鸣器状态
+    }
+}
