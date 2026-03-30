054d3e8614ac9f1ded2c5814eeb1aa2ea928d6dc
Hisoka6602 <91233659+Hisoka6602@users.noreply.github.com>
2026-03-30 23:56:40 +0800

    13

---
+++ Zeye.NarrowBeltSorter.Execution/Services/SortingTaskOrchestrationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Hosting;
+
+namespace Zeye.NarrowBeltSorter.Execution.Services {
+
+    public class SortingTaskOrchestrationService : BackgroundService {
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken) {
+            throw new NotImplementedException();
+        }
+    }
+}
