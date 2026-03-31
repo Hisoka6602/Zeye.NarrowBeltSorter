732949f91f18845c727dab76aa4dc35195bf7673
Hisoka6602 <91233659+Hisoka6602@users.noreply.github.com>
2026-04-01 01:24:32 +0800

    速度

--- Zeye.NarrowBeltSorter.Execution/Services/SortingTaskOrchestrationService.cs
+++ Zeye.NarrowBeltSorter.Execution/Services/SortingTaskOrchestrationService.cs
@@ -24,7 +24,7 @@ namespace Zeye.NarrowBeltSorter.Execution.Services {
         /// <summary>
         /// 包裹从创建到进入待装车队列的成熟延迟时间。
         /// </summary>
-        private static readonly TimeSpan ParcelMatureDelay = TimeSpan.FromMilliseconds(1950);
+        private static readonly TimeSpan ParcelMatureDelay = TimeSpan.FromMilliseconds(2500);
 
         /// <summary>
         /// 日志记录器。
